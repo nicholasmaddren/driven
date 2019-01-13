e2b82b25eb6889b62c2a669e7a73d3e5c285315d
nicholasmaddren <me@nickmaddren.com>
2019-01-13 17:30:28 +0000

    Adds BulletPoint component

---
+++ src/components/BulletPoints.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import * as React from 'react';
+
+const StyledBulletPoints = styled.div`
+  padding: 40px 80px;
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid ${props => props.theme.vars.color.grey2};
+`;
+
+const StyledtItem = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 100px;
+    height: 100px;
+    margin-right: 20px;
+  }
+`;
+
+interface IProps {
+  items: {
+    id: string;
+    heading: string;
+    description: string;
+    image: { src: string; alt: string };
+  }[];
+}
+
+const BulletPoints: React.SFC<IProps> = props => (
+  <StyledBulletPoints>
+    {props.items.map(item => {
+      return (
+        <StyledtItem key={item.id}>
+          <img src={item.image.src} alt={item.image.alt} />
+          <div className="content">
+            <h3>{item.heading}</h3>
+            <p>{item.description}</p>
+          </div>
+        </StyledtItem>
+      );
+    })}
+  </StyledBulletPoints>
+);
+
+export default BulletPoints;
