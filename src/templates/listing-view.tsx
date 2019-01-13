43eba1d9632aa80f3da19dc7396fcc3b67b8abcb
nicholasmaddren <me@nickmaddren.com>
2019-01-13 14:25:54 +0000

    Adds grid to Listing-view

--- src/templates/listing-view.tsx
+++ src/templates/listing-view.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
+
+const StyledListingView = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-gap: 40px;
+`;
+
+const StyledImageSlider = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+
+const StyledInfo = styled.div`
+  padding: 20px 0;
+`;
 
 export default ({ data }) => (
   <Layout>
-    <div>
-      <Link to="/cars">
-        Cars > {data.listings.make} {data.listings.model}
-      </Link>
-      <h3>{data.listings.make}</h3>
-      <p>{data.listings.model}</p>
-      <p>{data.listings.price}</p>
-      <p>{data.listings.mileage}</p>
-      <img src={data.listings.images[0]} />
-    </div>
+    <StyledListingView>
+      <StyledImageSlider>
+        <img src={data.listings.images[0]} />
+      </StyledImageSlider>
+      <StyledInfo>
+        <h3>{data.listings.make}</h3>
+        <p>{data.listings.model}</p>
+        <p>{data.listings.price}</p>
+        <p>{data.listings.mileage}</p>
+      </StyledInfo>
+    </StyledListingView>
   </Layout>
 );
 
