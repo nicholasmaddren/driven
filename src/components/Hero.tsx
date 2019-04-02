4970d4cbd021fb9f23141f3d96157633e81cc2da
nicholasmaddren <me@nickmaddren.com>
2019-04-02 21:25:16 +0100

    Adds extra Hero functionality

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,48 @@
 import styled from 'styled-components';
 import * as React from 'react';
 
+const getGetFlexPosition = (position: IContentPositions) => {
+  switch (position) {
+    case 'left':
+      return 'flex-start';
+    case 'center':
+      return 'center';
+    case 'right':
+      return 'flex-end';
+    default:
+      return 'flex-start';
+  }
+};
+
 const StyledHero = styled.div<ICommonProps>`
+  display: flex;
+  justify-content: ${({ contentPosition }) =>
+    getGetFlexPosition(contentPosition)};
   padding: 40px 80px;
+  text-align: ${({ contentPosition }) => contentPosition};
   background: linear-gradient(
-      ${({ bgGradientPosition }) => bgGradientPosition},
-      ${({ bgGradientColor1 }) => bgGradientColor1},
-      ${({ bgGradientColor2 }) => bgGradientColor2}
+      ${({ bgColorPosition }) => bgColorPosition},
+      ${({ bgColor1 }) => bgColor1},
+      ${({ bgColor2, bgColor1 }) => (bgColor2 ? bgColor2 : bgColor1)}
     ),
     url(${({ bgImage }) => bgImage}) no-repeat ${({ bgPosition }) => bgPosition};
   background-size: cover;
 `;
 
-const StyledContent = styled.div`
-  color: #fff;
-  margin-bottom: 40px;
+const StyledContainer = styled.div<ICommonProps>`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: ${({ contentPosition }) =>
+    getGetFlexPosition(contentPosition)};
   width: 50%;
+`;
+
+const StyledContent = styled.div<ICommonProps>`
+  color: ${({ textColor }) => textColor};
   font-weight: 600;
   line-height: 1.2;
   text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
+  margin-bottom: 40px;
   h1 {
     font-size: 55px;
   }
@@ -28,12 +52,16 @@ const StyledContent = styled.div`
   }
 `;
 
+type IContentPositions = 'left' | 'center' | 'right';
+
 interface ICommonProps {
+  contentPosition?: IContentPositions;
   bgImage?: string;
   bgPosition?: string;
-  bgGradientColor1?: string;
-  bgGradientColor2?: string;
-  bgGradientPosition?: string;
+  bgColor1?: string;
+  bgColor2?: string;
+  bgColorPosition?: string;
+  textColor?: string;
 }
 
 interface IHeroProps extends ICommonProps {
@@ -42,22 +70,26 @@ interface IHeroProps extends ICommonProps {
 }
 
 const defaultProps: ICommonProps = {
-  bgGradientPosition: '0deg',
+  bgColorPosition: '0deg',
+  textColor: '#fff',
 };
 
 const Hero: React.SFC<IHeroProps> = props => (
   <StyledHero
+    contentPosition={props.contentPosition}
     bgImage={props.bgImage}
     bgPosition={props.bgPosition}
-    bgGradientColor1={props.bgGradientColor1}
-    bgGradientColor2={props.bgGradientColor2}
-    bgGradientPosition={props.bgGradientPosition}
+    bgColor1={props.bgColor1}
+    bgColor2={props.bgColor2}
+    bgColorPosition={props.bgColorPosition}
   >
-    <StyledContent>
-      {props.heading && <h1>{props.heading}</h1>}
-      {props.paragraph && <p>{props.paragraph}</p>}
-    </StyledContent>
-    {props.children}
+    <StyledContainer contentPosition={props.contentPosition}>
+      <StyledContent textColor={props.textColor}>
+        {props.heading && <h1>{props.heading}</h1>}
+        {props.paragraph && <p>{props.paragraph}</p>}
+      </StyledContent>
+      {props.children}
+    </StyledContainer>
   </StyledHero>
 );
 
