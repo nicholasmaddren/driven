b1dfda4c22dc3c7a97bff5d8c4325c6eb98ce799
nicholasmaddren <me@nickmaddren.com>
2019-04-04 21:37:49 +0100

    Adds Footer component and imports into layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,143 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faYoutube,
+  faFacebook,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+const StyledFooter = styled.footer<ICommonProps>`
+  background-color: ${({ bgColor }) => bgColor};
+  color: ${({ textColor }) => textColor};
+  a {
+    color: ${({ textColor }) => textColor};
+  }
+  hr {
+    height: 1px;
+    width: 100%;
+    background: ${({ theme }) => theme.vars.color.grey4};
+    border: 0;
+    margin: 30px 0;
+  }
+`;
+
+const StyledContainer = styled.div`
+  max-width: 1200px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 50px;
+  margin: 0 auto;
+`;
+
+const StyledFooterBrand = styled.div`
+  margin-bottom: 30px;
+  .logo {
+    max-width: 150px;
+    display: block;
+  }
+`;
+
+const StyledContactInfo = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    font-size: 12px;
+    padding: 6px;
+    border-radius: 100%;
+    border: 2px solid #fff;
+    margin-right: 10px;
+  }
+  a {
+    font-size: 22px;
+    font-weight: 600;
+  }
+`;
+
+const StyledSocialLinks = styled.ul`
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+  li {
+    margin-right: 20px;
+    svg {
+      font-size: 20px;
+    }
+  }
+`;
+
+const StyledPageLinks = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  list-style-type: none;
+  padding: 0;
+  li {
+    margin-bottom: 20px;
+  }
+`;
+
+interface ICommonProps {
+  bgColor: string;
+  textColor: string;
+}
+
+interface IFooterProps extends ICommonProps {
+  pageLinks: { name: string; to: string }[];
+  logoImage: string;
+  siteTitle: string;
+}
+
+const Footer: FC<IFooterProps> = props => (
+  <StyledFooter bgColor={props.bgColor} textColor={props.textColor}>
+    <StyledContainer>
+      <div>
+        <StyledFooterBrand>
+          <img className="logo" src={props.logoImage} alt={props.siteTitle} />
+          <small>© 2019 DRIVEN GROUP. ALL RIGHTS RESERVED.</small>
+        </StyledFooterBrand>
+
+        <StyledSocialLinks>
+          <li>
+            <Link to="/">
+              <FontAwesomeIcon icon={faFacebook} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FontAwesomeIcon icon={faTwitter} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FontAwesomeIcon icon={faYoutube} />
+            </Link>
+          </li>
+        </StyledSocialLinks>
+      </div>
+      <div>
+        <StyledContactInfo>
+          <FontAwesomeIcon icon={faPhone} flip="horizontal" />
+          <a>01642 890900</a>
+        </StyledContactInfo>
+        <hr />
+        <StyledPageLinks>
+          {props.pageLinks.map(pageLink => (
+            <li>
+              <Link to={pageLink.to}>{pageLink.name}</Link>
+            </li>
+          ))}
+        </StyledPageLinks>
+      </div>
+    </StyledContainer>
+  </StyledFooter>
+);
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,7 @@ import { normalize } from 'styled-normalize';
 
 import theme from '../theme';
 import Header from './Header';
+import Footer from './Footer';
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
@@ -12,7 +13,8 @@ const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700');
     font-family: ${theme.vars.font.family};
     color: ${theme.vars.color.grey4};
-    font-size: 14px;
+    background-color: ${theme.vars.color.grey1};
+    font-size: 16px;
   }
 
   h1, h2, h3, h4, h5, h6 {
@@ -52,6 +54,19 @@ const Layout: React.SFC = props => (
             logoImage="https://drivengroup.co.uk/wp-content/themes/driven/dist/images/logo-dark_1125fb4a.png"
           />
           <div>{props.children}</div>
+          <Footer
+            siteTitle={data.site.siteMetadata.title}
+            logoImage="https://drivengroup.co.uk/wp-content/themes/driven/dist/images/logo_aa5ba1d7.png"
+            bgColor="#041022"
+            textColor="#fff"
+            pageLinks={[
+              { name: 'Home', to: '/' },
+              { name: 'Cars For Sale', to: '/cars' },
+              { name: 'Sell Your Car', to: '/sell-your-car' },
+              { name: 'About Us', to: '/about-us' },
+              { name: 'Contact', to: '/contact' },
+            ]}
+          />
         </>
       )}
     />
