a04c649124b1fc411c8c60251f52e288207f1f6f
nicholasmaddren <me@nickmaddren.com>
2019-04-14 21:41:20 +0100

    gatsby-node refactor

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,38 +12,11 @@ exports.sourceNodes = async ({ actions }) => {
   const { createNode, createPage } = actions;
   await axios
     .get(`${process.env.API_URL}/cars/external/${process.env.DEALERSHIP_ID}`, {
-      headers: { Authorization: `Bearer ${process.env.TOKEN}` },
+      headers: {
+        Authorization: `Bearer fmEB*cuY.]zwA!tby^PR_G<}{*EtI?°&h|hmdPm%ityuA<1|B(7T§TBEjwrXrDy6`,
+      },
     })
     .then(res => {
-      if (!res.data.length) {
-        res.data.push({
-          vin: '12345',
-          make: 'BMW',
-          model: '3 Series',
-          trim: '320d M Sport',
-          description: 'Lovely example',
-          color: 'red',
-          price: 14895,
-          year: 2016,
-          mileage: 22308,
-          doors: 5,
-          seats,
-          bodyType: 'Saloon',
-          fuelType: 'Diesel',
-          engineSize: 1995,
-          transmission: 'Automatic',
-          bhp: 201,
-          torque: 230,
-          zeroTo60: 7.6,
-          condition: 'Used',
-          interiorFeatures: 'Blutooth - Heated Seats - Air Conditioning',
-          exteriorFeatures: 'Alloy Wheels - Xeonon Headlights',
-          images: [
-            'https://inchcapecdn.azureedge.net/cdn-images/stock/a/au68jsy-1-xl.jpg',
-          ],
-          slug: 'cars/bmw-3-series-12345',
-        });
-      }
       res.data.map(car => {
         const {
           vin,
@@ -128,45 +101,103 @@ exports.sourceNodes = async ({ actions }) => {
         });
       });
     })
-    .catch(e => console.log(e));
-
-  (async function() {
-    console.log('Starting config gatsby-node');
-    // fetch raw data from the dealership-info api
-    axios
-      .get(
-        `${process.env.API_URL}/dealership/external/config/${
-          process.env.DEALERSHIP_ID
-        }`,
-        {
-          headers: { Authorization: `Bearer ${process.env.TOKEN}` },
-        }
-      )
-      .then(res => {
-        // Create your node object
-        const configNode = {
+    .catch(error => {
+      console.log(error.response.status);
+      if (error.response.status === 404) {
+        const dummyNode = {
           // Required fields
-          id: 'config',
+          id: 'car-12345',
           parent: `__SOURCE__`,
           internal: {
-            type: `Config`,
+            type: `Cars`, // name of the graphQL query --> allCars {}
           },
           children: [],
-          ...res.data,
-        };
 
+          vin: '12345',
+          make: 'BMW',
+          model: '3 Series',
+          trim: '320d M Sport',
+          description: 'Lovely example',
+          color: 'red',
+          price: 14895,
+          year: 2016,
+          mileage: 22308,
+          doors: 5,
+          seats: 5,
+          bodyType: 'Saloon',
+          fuelType: 'Diesel',
+          engineSize: 1995,
+          transmission: 'Automatic',
+          bhp: 201,
+          torque: 230,
+          zeroTo60: 7.6,
+          condition: 'Used',
+          interiorFeatures: ['Blutooth', 'Heated Seats', 'Air Conditioning'],
+          exteriorFeatures: ['Alloy Wheels', 'Xeonon Headlights'],
+          images: [
+            'https://inchcapecdn.azureedge.net/cdn-images/stock/a/au68jsy-1-xl.jpg',
+          ],
+          slug: 'cars/bmw-3-series-12345',
+        };
         // Get content digest of node. (Required field)
         const contentDigest = crypto
           .createHash(`md5`)
-          .update(JSON.stringify(configNode))
+          .update(JSON.stringify(dummyNode))
           .digest(`hex`);
         // add it to userNode
-        configNode.internal.contentDigest = contentDigest;
+        dummyNode.internal.contentDigest = contentDigest;
 
         // Create node with the gatsby createNode() API
-        createNode(configNode);
-      })
-      .catch(e => console.log(e));
-  })();
+        createNode(dummyNode);
+
+        // Create page for node
+
+        createPage({
+          path: dummyNode.slug,
+          component: path.resolve(`./src/templates/listing-view.tsx`),
+          context: {
+            slug: dummyNode.slug,
+          },
+        });
+      }
+    });
+
+  // fetch raw data from the dealership-info api
+  await axios
+    .get(
+      `${process.env.API_URL}/dealership/external/config/${
+        process.env.DEALERSHIP_ID
+      }`,
+      {
+        headers: {
+          Authorization: `Bearer fmEB*cuY.]zwA!tby^PR_G<}{*EtI?°&h|hmdPm%ityuA<1|B(7T§TBEjwrXrDy6`,
+        },
+      }
+    )
+    .then(res => {
+      // Create your node object
+      const configNode = {
+        // Required fields
+        id: 'config',
+        parent: `__SOURCE__`,
+        internal: {
+          type: `Config`,
+        },
+        children: [],
+        ...res.data,
+      };
+
+      // Get content digest of node. (Required field)
+      const contentDigest = crypto
+        .createHash(`md5`)
+        .update(JSON.stringify(configNode))
+        .digest(`hex`);
+      // add it to userNode
+      configNode.internal.contentDigest = contentDigest;
+
+      // Create node with the gatsby createNode() API
+      createNode(configNode);
+    })
+    .catch(e => console.log(e));
   return;
 };
