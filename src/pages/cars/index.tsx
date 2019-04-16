da26daf416abbf6a4ab85674adaae3d1bba058e4
nicholasmaddren <me@nickmaddren.com>
2019-04-16 23:05:21 +0100

    Changes cars index name

--- src/pages/cars/index.tsx
+++ src/pages/cars/index.tsx
@@ -215,7 +215,7 @@ class Cars extends React.Component<IProps> {
         <InstantSearch
           appId="UQS7NGYH01"
           apiKey="417b0c1b13d3941a26d83069eec84360"
-          indexName={process.env.SITE_NAME}
+          indexName={`${process.env.DEALERSHIP_ID}-cars`}
           searchState={this.state.searchState}
           onSearchStateChange={event => this.onSearchStateChangedHandler(event)}
           createURL={event => this.createURL(event)}
